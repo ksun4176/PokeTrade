fe182b0ba8bd1ea5a45e5f2c18a9f2bafb676399
unknown <ksun4176@gmail.com>
2025-02-12 23:12:09 -0500

    lock accounts lookup behind auth

--- api/src/user/controllers/user.controller.ts
+++ api/src/user/controllers/user.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Inject, Param } from "@nestjs/common";
+import { Controller, Get, Inject, UseGuards } from "@nestjs/common";
 import { Routes, Services } from "src/utils/constants";
 import { IAccountService } from "src/account/services/account.service";
+import { AuthUser } from "src/utils/decorators";
+import { players } from "@prisma/client";
+import { AuthenticatedGuard } from "src/utils/Guards";
 
 @Controller(Routes.USER)
 export class UserController {
@@ -8,12 +11,11 @@ export class UserController {
     @Inject(Services.ACCOUNT) private readonly accountService: IAccountService,
   ) { }
 
-  @Get('users/:userId/accounts')
-  getAccounts(@Param('userId') userId: string) {
-    const userIdInt = parseInt(userId);
-    if (!userIdInt) return [];
+  @Get('users/accounts')
+  @UseGuards(AuthenticatedGuard)
+  getAccounts(@AuthUser() user: players) {
     return this.accountService.getAccounts({
-      player_id: userIdInt,
+      player_id: user.id,
     });
   }
 }
